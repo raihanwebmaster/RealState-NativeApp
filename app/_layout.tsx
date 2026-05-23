cf643acdb960002600da60abbc811eb4785f16e0
raihanwebmaster <raihanemon2015@gmail.com>
2026-05-23 12:13:18 +0200

    feat: implement tab navigation with Home, Search, Saved, Profile screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,6 @@
-import { Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Slot } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-  return (
-    <SafeAreaView className="bg-black" >
-      <View>
-        <Text>Root Layout</Text>
-      </View>
-    </SafeAreaView>
-  )
+  return <Slot />;
 }
