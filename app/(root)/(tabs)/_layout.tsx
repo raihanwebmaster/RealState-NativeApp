d3b8f543ca90a32c95c6e0f5deba4678049aaf5d
raihanwebmaster <raihanemon2015@gmail.com>
2026-05-24 11:02:17 +0200

    feat: enhance tab layout with vector icons for better platform compatibility

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,24 +1,25 @@
-import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Icon, Label, NativeTabs, VectorIcon } from 'expo-router/unstable-native-tabs';
 
 export default function TabLayout() {
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
         <Label>Home</Label>
-        <Icon sf="house.fill" />
+        <Icon sf="house.fill" androidSrc={<VectorIcon family={Ionicons} name="home" />} />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="search">
-        <Icon sf="magnifyingglass" />
+        <Icon sf="magnifyingglass" androidSrc={<VectorIcon family={Ionicons} name="search" />} />
         <Label>Search</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="saved">
-        <Icon sf="heart.fill"  />
+        <Icon sf="heart.fill" androidSrc={<VectorIcon family={Ionicons} name="heart" />} />
         <Label>Saved</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="profile">
-        <Icon sf="person.fill" />
+        <Icon sf="person.fill" androidSrc={<VectorIcon family={Ionicons} name="person" />} />
         <Label>Profile</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
