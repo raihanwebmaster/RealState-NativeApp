d1b368d123fbb38d0097208bd0830d1ca10e03e0
raihanwebmaster <raihanemon2015@gmail.com>
2026-05-24 15:33:01 +0200

    feat: implement tab navigation with admin-specific options and create screen

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -1,27 +1,87 @@
+import { useUserStore } from '@/store/userStore';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { Icon, Label, NativeTabs, VectorIcon } from 'expo-router/unstable-native-tabs';
+import { Tabs } from 'expo-router';
+import { Icon, Label, NativeTabs } from 'expo-router/unstable-native-tabs';
+import { Platform } from 'react-native';
 
-export default function TabLayout() {
+function IOSTabs() {
+  const isAdmin = useUserStore((state) => state.isAdmin);
   return (
     <NativeTabs>
       <NativeTabs.Trigger name="index">
         <Label>Home</Label>
-        <Icon sf="house.fill" androidSrc={<VectorIcon family={Ionicons} name="home" />} />
+        <Icon sf="house.fill" />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="search">
-        <Icon sf="magnifyingglass" androidSrc={<VectorIcon family={Ionicons} name="search" />} />
+        <Icon sf="magnifyingglass" />
         <Label>Search</Label>
       </NativeTabs.Trigger>
 
+      {
+        isAdmin && (
+          <NativeTabs.Trigger name="create">
+            <Icon sf="plus.circle.fill" />
+            <Label>Add Property</Label>
+          </NativeTabs.Trigger>
+        )
+      }
+
       <NativeTabs.Trigger name="saved">
-        <Icon sf="heart.fill" androidSrc={<VectorIcon family={Ionicons} name="heart" />} />
+        <Icon sf="heart.fill" />
         <Label>Saved</Label>
       </NativeTabs.Trigger>
 
       <NativeTabs.Trigger name="profile">
-        <Icon sf="person.fill" androidSrc={<VectorIcon family={Ionicons} name="person" />} />
+        <Icon sf="person.fill" />
         <Label>Profile</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
   );
+}
+
+function AndroidTabs() {
+  const isAdmin = useUserStore((state) => state.isAdmin);
+  return (
+    <Tabs screenOptions={{ headerShown: false, }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
+          tabBarLabel: 'Home'
+        }} />
+      <Tabs.Screen
+        name="search"
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="search" size={size} color={color} />,
+          tabBarLabel: 'Search'
+        }} />
+      {
+        isAdmin && (
+          <Tabs.Screen
+            name="create"
+            options={{
+              tabBarIcon: ({ color, size }) => <Ionicons name="add" size={size} color={color} />,
+              tabBarLabel: 'Add Property'
+            }} />
+        )
+      }
+      <Tabs.Screen
+        name="saved"
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="heart" size={size} color={color} />,
+          tabBarLabel: 'Saved'
+        }} />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarIcon: ({ color, size }) => <Ionicons name="person" size={size} color={color} />,
+          tabBarLabel: 'Profile'
+        }} />
+    </Tabs>
+  );
+}
+
+
+export default function TabLayout() {
+  return Platform.OS === 'ios' ? <IOSTabs /> : <AndroidTabs />;
 }
